46345e474d5e425db5e325277c6b06380422194e
Vikram Ramavarapu <vikramr2@illinois.edu>
2022-11-30 17:57:48 -0600

    MAGUS decomposer

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -r magus_decompose/__pycache__
+rm -r magus_decompose/magus_working_dir
+rm -r magus_decompose/magus_align/__pycache__
+rm -r magus_decompose/magus_helpers/__pycache__
+rm -r magus_decompose/magus_tasks/__pycache__
+rm -r magus_decompose/magus_tools/__pycache__
